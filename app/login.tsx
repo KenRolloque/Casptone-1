0014e65fc30c857f88620c1b697d7ac722542b4c
Ken <orochi1418@gmail.com>
2023-09-17 23:28:13 +0800

    V3

---
+++ app/login.tsx
@@ -0,0 +1,40 @@
+// import * as React from 'react';
+// import { Text, View, StyleSheet, Button } from 'react-native';
+// import { registerRootComponent } from "expo"
+// import { Link, Stack, useRootNavigationState, Redirect, router} from 'expo-router';
+
+
+// const componentName = () => {
+
+//     const login = () =>{
+
+//         router.replace('/(tabs)/gallery')
+//       }
+//   return (
+
+
+
+
+//       <View style={styles.container}>
+//         <Text >This screen doesn't exist.</Text>
+
+//         <Link href="/" >
+//           <Text >Go to home screen!</Text>
+//         </Link>
+//         <Button title='go' onPress={login}/>
+//       </View>
+      
+//   );
+// };
+
+
+
+// const styles = StyleSheet.create({
+//   container: {}
+// });
+
+// registerRootComponent (componentName)
+
+
+
+
